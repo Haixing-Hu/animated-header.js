b626386f3588928e78d6a9ff231370d8d38e72c5
Haixing Hu <starfish.hu@gmail.com>
2015-03-18 23:33:53 +0800

    Finish the v1.0.0

---
+++ js/animated-header.js
@@ -0,0 +1,44 @@
+/**
+ * animated-header.js v1.0.0
+ * https://github.com/Haixing-Hu/animated-header.js
+ *
+ * Licensed under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+ *
+ * Copyright 2013, Codrops, http://www.codrops.com
+ * Copyright 2015, Haixing Hu, https://github.com/Haixing-Hu/
+ */
+(function($) {
+
+	var docElem = document.documentElement,
+		header = $('.animated-header'),
+		didScroll = false,
+		changeHeaderOn = 300;
+
+	function init() {
+    console.debug("init");
+		window.addEventListener('scroll', function(event) {
+			if( !didScroll ) {
+				didScroll = true;
+				setTimeout( scrollPage, 250 );
+			}
+		}, false );
+	}
+
+	function scrollPage() {
+    console.debug("scrollPage");
+		var sy = scrollY();
+		if (sy >= changeHeaderOn) {
+			header.addClass('animated-header-scroll');
+		} else {
+			header.removeClass('animated-header-scroll');
+		}
+		didScroll = false;
+	}
+
+	function scrollY() {
+		return window.pageYOffset || docElem.scrollTop;
+	}
+
+	init();
+})(jQuery);
