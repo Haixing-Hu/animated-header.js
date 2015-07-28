379a172e088f400d5879f08ca18c2a17ff3fe848
Haixing Hu <starfish.hu@gmail.com>
2015-07-28 20:45:45 +0800

    fix a potential bug when used the script with Vue's component system

--- js/animated-header.js
+++ js/animated-header.js
@@ -1,5 +1,5 @@
 /**
- * animated-header.js v1.0.0
+ * animated-header.js v1.0.3
  * https://github.com/Haixing-Hu/animated-header.js
  *
  * Licensed under the MIT license.
@@ -10,33 +10,32 @@
  */
 (function($) {
 
-	var docElem = document.documentElement,
-		header = $('.animated-header'),
-		didScroll = false,
-		changeHeaderOn = 300;
+  var didScroll = false;
+  var changeHeaderOn = 300;
 
-	function init() {
-		window.addEventListener('scroll', function(event) {
-			if( !didScroll ) {
-				didScroll = true;
-				setTimeout( scrollPage, 250 );
-			}
-		}, false );
-	}
+  function init() {
+    window.addEventListener('scroll', function(event) {
+      if( !didScroll ) {
+        didScroll = true;
+        setTimeout( scrollPage, 250 );
+      }
+    }, false );
+  }
 
-	function scrollPage() {
-		var sy = scrollY();
-		if (sy >= changeHeaderOn) {
-			header.addClass('animated-header-scroll');
-		} else {
-			header.removeClass('animated-header-scroll');
-		}
-		didScroll = false;
-	}
+  function scrollPage() {
+    var sy = scrollY();
+    var header = $('.animated-header');
+    if (sy >= changeHeaderOn) {
+      header.addClass('animated-header-scroll');
+    } else {
+      header.removeClass('animated-header-scroll');
+    }
+    didScroll = false;
+  }
 
-	function scrollY() {
-		return window.pageYOffset || docElem.scrollTop;
-	}
+  function scrollY() {
+    return window.pageYOffset || document.documentElement.scrollTop;
+  }
 
-	init();
+  init();
 })(jQuery);
